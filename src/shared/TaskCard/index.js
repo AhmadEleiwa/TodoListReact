facc4c97751cf08d78c21a44b87da7148de9c543
ahmadeleiwa <ahmadilawa@gmail.com>
2023-04-30 19:46:50 +0300

    Card background color are taken from theme

--- src/shared/TaskCard/index.js
+++ src/shared/TaskCard/index.js
@@ -9,24 +9,39 @@ import Theme from '../../utils/Theme'
 const TaskCard = props => {
     const [inputData, setInputData] = useState(props.value)
     const [inputEnable, setInputEnable] = useState(props.inputEnable)
+    const [taskEnable, setTaskEnable] = useState(props.taskEnable)
     const theme = useContext(Theme)
 
     const deleteTaskHandler = () => {
         // delete task logic
     }
+    const editTask = (e) => {
+        setInputData(e.target.value)
+    }
 
+    const enableEdit = () => {
+        if (taskEnable !== true)
+            setInputEnable(!inputEnable)
+    }
+    
+    const taskCheckHandler = () => {
+        setInputEnable(true)
+        setTaskEnable(!taskEnable)
+    }
 
     return <div className='task-card'
         style={{
-            backgroundColor: theme.paper
+            backgroundColor: taskEnable ? theme.pallete.disabled : theme.pallete.paper
         }}>
-        <CheckBox />
+        <CheckBox onClick={ taskCheckHandler} />
         <div className='task-body' >
-            <input style={{ color: theme.pallete.main }} onChange={(e) => { setInputData(e.target.value) }} disabled={inputEnable} value={inputData} />
+            <input style={{ color: theme.pallete.main , border:`1px solid ${theme.pallete.paperBorder}`}} onChange={editTask} disabled={inputEnable} value={inputData} />
             <p>{props.assignee}</p>
         </div>
-        <FontAwesomeIcon onClick={() => { setInputEnable(!inputEnable) }}  icon={faEdit} color={inputEnable ? theme.pallete.main: theme.pallete.buttonDisabled} />
-        <FontAwesomeIcon icon={faTrash} color={'red'} />
+        <div className='control'>
+            <FontAwesomeIcon icon={faEdit} onClick={enableEdit} color={!taskEnable ? theme.pallete.main : theme.pallete.buttonDisabled} />
+            <FontAwesomeIcon icon={faTrash} color={'red'} />
+        </div>
     </div>
 }
 
