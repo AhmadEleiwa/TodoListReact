d275f2065fbb17861b0e1090d788141c8a0eb01c
ahmadeleiwa <ahmadilawa@gmail.com>
2023-04-30 19:22:18 +0300

    Use theme color

--- src/shared/CheckBox/index.js
+++ src/shared/CheckBox/index.js
@@ -4,17 +4,21 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
 
 
-import { useState } from 'react'
+import { useContext, useState } from 'react'
+import Theme from '../../utils/Theme'
 const CheckBox = props => {
     const [isChecked, setIsChecked] = useState(false)
+
+    const theme = useContext(Theme)
+
     const clickHandler = () =>{
         setIsChecked(!isChecked)
         if(props.onClick)
             props.onClick() 
     }
     return <>
-        {isChecked ? <FontAwesomeIcon onClick={clickHandler} fontWeight={'100%'}  icon={faCheckCircle} color='#11A8E6' fontSize={'32px'} />
-        :<FontAwesomeIcon onClick={clickHandler}  className='empty-cricle' fontWeight={'100%'}  icon={faCheckCircle} color='transparent' fontSize={'31px'} />
+        {isChecked ? <FontAwesomeIcon onClick={clickHandler} fontWeight={'100%'}  icon={faCheckCircle} color={theme.pallete.main} fontSize={'32px'} />
+        :<FontAwesomeIcon onClick={clickHandler}  className='empty-cricle' fontWeight={'100%'}  icon={faCheckCircle} color='transparent' fontSize={'32px'} />
         }
     </>
 
