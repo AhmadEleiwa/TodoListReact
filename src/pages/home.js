4552aefd208e4680c4e9b2ea1fa5d3f0b6222fc9
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-01 15:16:52 +0300

    Handling the DataContext with localStorage & ADD the dataContext provider

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,28 @@
+
+import {useState } from "react"
+import Header from "../shared/Header"
+import TaskList from "../shared/TaskList"
+import { load, store } from "../utils/localStorageUtils"
+
+import DataContext from '../utils/localStorageUtils'
 const HomePage = props =>{
-    return <></>
+    const [dataLocal, setDataLocal] = useState(load('data'))
+
+    const dataHandler = (data) => {
+        // setState
+        setDataLocal(data)
+   
+        // store into localStorage
+        store('data', data)
+    }
+    return <DataContext.Provider value={{
+        data: dataLocal? dataLocal : [], 
+        changeHandler:dataHandler
+    }}>
+     <Header />
+     <TaskList  />
+
+    </ DataContext.Provider>
 }
 
 export default HomePage
