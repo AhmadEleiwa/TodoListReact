aa2b7ac467c70c45afcad21d6393d8fa1eb01321
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-01 23:55:49 +0300

    Add statistics component and handler its functionilies

---
+++ src/shared/Statisics/index.js
@@ -0,0 +1,17 @@
+import TitledCard from '../TitledCard'
+import { faCheckToSlot, faCheck, faClock, faTrash} from '@fortawesome/free-solid-svg-icons'
+import { useContext } from 'react'
+import DataContext from '../../utils/localStorageUtils'
+const Statsics = props =>{
+    const dataContext = useContext(DataContext)
+    return <div>
+        <TitledCard title='TODOS' value={dataContext.stastics.all} icon={faCheckToSlot} />
+        <TitledCard title='Done' value={dataContext.stastics.done} icon={faCheck} />
+        <TitledCard title='PENDING' value={dataContext.stastics.pending}  icon={faClock} />
+        <TitledCard title='DELETED' value={dataContext.stastics.deleted} icon={faTrash} />
+
+
+    </div>
+}
+
+export default Statsics
