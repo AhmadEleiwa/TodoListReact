00c1a6256a76e166a8b66c4d916fb777007f3c33
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-01 15:35:32 +0300

    clean up

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,26 +1,30 @@
 
-import {useState } from "react"
+import { useState } from "react"
 import Header from "../shared/Header"
 import TaskList from "../shared/TaskList"
 import { load, store } from "../utils/localStorageUtils"
 
 import DataContext from '../utils/localStorageUtils'
-const HomePage = props =>{
+
+const HomePage = props => {
     const [dataLocal, setDataLocal] = useState(load('data'))
 
     const dataHandler = (data) => {
         // setState
         setDataLocal(data)
-   
+
         // store into localStorage
         store('data', data)
     }
+
+
+
     return <DataContext.Provider value={{
-        data: dataLocal? dataLocal : [], 
-        changeHandler:dataHandler
+        data: dataLocal ? dataLocal : [],
+        changeHandler: dataHandler
     }}>
-     <Header />
-     <TaskList  />
+        <Header />
+        <TaskList />
 
     </ DataContext.Provider>
 }
