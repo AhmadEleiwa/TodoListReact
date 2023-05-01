7f2e6a5c937549215860f41aab9049d0f5a0e42c
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-01 23:11:10 +0300

    enableTask from parent

--- src/shared/TaskCard/index.js
+++ src/shared/TaskCard/index.js
@@ -9,7 +9,6 @@ import DataContext from '../../utils/localStorageUtils'
 
 const TaskCard = props => {
     const [inputData, setInputData] = useState(props.value)
-    const [taskEnable, setTaskEnable] = useState(props.taskEnable)
 
     const theme = useContext(Theme)
     const dataContext = useContext(DataContext)
@@ -29,7 +28,7 @@ const TaskCard = props => {
     }
 
     const enableEdit = () => {
-        if (taskEnable !== true) {
+        if (props.taskEnable !== true) {
             props.setIndexHandler(props.id)
         }
     }
@@ -37,23 +36,22 @@ const TaskCard = props => {
     const taskCheckHandler = () => {
         props.setIndexHandler(-1)
         const i = dataContext.data.findIndex(p => p.id === props.id)
-        setTaskEnable(!taskEnable)
-        dataContext.data[i].state = !taskEnable
+        dataContext.data[i].state = !props.taskEnable
         dataContext.changeHandler(dataContext.data)
     }
 
     return <div className='task-card'
         style={{
-            backgroundColor: taskEnable ? theme.pallete.disabled : theme.pallete.paper,
+            backgroundColor: props.taskEnable ? theme.pallete.disabled : theme.pallete.paper,
             boxShadow:`0 2px 8px 0 ${theme.pallete.dropShadow}`
         }}>
 
-        <CheckBox onClick={taskCheckHandler} checked={taskEnable} />
+        <CheckBox onClick={taskCheckHandler} checked={props.taskEnable} />
         <div className='task-body' >
             <input style={{ 
                 color: theme.pallete.main, 
                 border: `1px solid ${theme.pallete.paperBorder}`, 
-                textDecoration: taskEnable ? "line-through" : "none" }} 
+                textDecoration: props.taskEnable ? "line-through" : "none" }} 
                 onChange={editTask} disabled={props.inputEnable} 
                 value={inputData} />
             <p style={{ color: theme.pallete.textSecondary }}>{props.assignee}</p>
@@ -62,7 +60,7 @@ const TaskCard = props => {
             <FontAwesomeIcon 
             icon={faEdit} 
             onClick={enableEdit} 
-            color={!taskEnable ? theme.pallete.main : theme.pallete.buttonDisabled} />
+            color={!props.taskEnable ? theme.pallete.main : theme.pallete.buttonDisabled} />
             <FontAwesomeIcon 
             onClick={deleteTaskHandler} 
             icon={faTrash} 
