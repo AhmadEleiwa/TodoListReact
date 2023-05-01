52603e3ddb90877a68ec41b50d2b4d8f5a2eb5d8
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-01 23:59:48 +0300

    Adding styling into statisics component

--- src/shared/Statisics/index.js
+++ src/shared/Statisics/index.js
@@ -2,15 +2,18 @@ import TitledCard from '../TitledCard'
 import { faCheckToSlot, faCheck, faClock, faTrash} from '@fortawesome/free-solid-svg-icons'
 import { useContext } from 'react'
 import DataContext from '../../utils/localStorageUtils'
+
+import './style.css'
+
 const Statsics = props =>{
     const dataContext = useContext(DataContext)
-    return <div>
+
+
+    return <div className='statisics-list'>
         <TitledCard title='TODOS' value={dataContext.stastics.all} icon={faCheckToSlot} />
         <TitledCard title='Done' value={dataContext.stastics.done} icon={faCheck} />
         <TitledCard title='PENDING' value={dataContext.stastics.pending}  icon={faClock} />
         <TitledCard title='DELETED' value={dataContext.stastics.deleted} icon={faTrash} />
-
-
     </div>
 }
 
