c73fec5ccb61721fac0f5246983f9cbdbf542cf3
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-01 13:11:57 +0300

    border color from theme

--- src/shared/CheckBox/index.js
+++ src/shared/CheckBox/index.js
@@ -18,7 +18,7 @@ const CheckBox = props => {
     }
     return <>
         {isChecked ? <FontAwesomeIcon onClick={clickHandler} fontWeight={'100%'}  icon={faCheckCircle} color={theme.pallete.main} fontSize={'32px'} />
-        :<FontAwesomeIcon onClick={clickHandler}  className='empty-cricle' style={{borderColor:theme.pallete.textPrimary}} fontWeight={'100%'}  icon={faCheckCircle} color='transparent' fontSize={'32px'} />
+        :<FontAwesomeIcon onClick={clickHandler}  className='empty-cricle' style={{borderColor:theme.pallete.textSecondary}} fontWeight={'100%'}  icon={faCheckCircle} color='transparent' fontSize={'32px'} />
         }
     </>
 
