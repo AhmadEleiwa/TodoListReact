f56e062de044d805c03f0ead01135626891c9c5a
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-18 18:40:21 +0300

    Handler the controllButtons with apis

--- src/shared/ControlButtons/index.js
+++ src/shared/ControlButtons/index.js
@@ -1,81 +1,83 @@
-import {useRef, useState } from 'react'
-import Button from '../Button'
-import Model from '../Model'
+import { useRef, useState } from "react";
+import Button from "../Button";
+import Model from "../Model";
 
-import './style.css'
-import { useData } from '../../contexts/DataContext'
+import "./style.css";
+import { useData } from "../../contexts/DataContext";
 
-const ControlButtons = props => {
-    const [modelOpen, setModelOpen] = useState(false)
+const ControlButtons = (props) => {
+  const [modelOpen, setModelOpen] = useState(false);
 
-    const dataContext = useData()
+  const dataContext = useData();
 
-    const handler = useRef({ title: '', handler: null })
+  const handler = useRef({ title: "", handler: null });
 
-    const markAllHandler = () => {
-        let data = [...dataContext.data]
-        data = data.map((i) => {
-            return { ...i, state: true }
-        })
-        dataContext.changeHandler(data)
+  const markAllHandler = () => {
+    let data = [...dataContext.data];
+    data = data.map((i) => {
+      return { ...i, status: true };
+    });
+    dataContext.updateTodo(data);
+  };
 
-        dataContext.statHandler({ ...dataContext.stastics, all: data.length, done: data.length, pending: 0 })
-    }
+  const marAsUndone = () => {
+    let data = [...dataContext.data];
+    data = data.map((i) => {
+      return { ...i, status: false };
+    });
+    dataContext.updateTodo(data);
+  };
 
-    const marAsUndone = () => {
-        let data = [...dataContext.data]
-        data = data.map((i) => {
-            return { ...i, state: false }
-        })
-        dataContext.changeHandler(data)
+  const clearDoneTask = () => {
+    let data = dataContext.data.filter((i) => i.status === true);
+    dataContext.deleteTodo(data);
+  };
+  const clearAll = () => {
+    dataContext.deleteTodo(dataContext.data);
+  };
+  const groupByAssignee = () => {
+    props.groupByAssignee();
+  };
 
-        dataContext.statHandler({ ...dataContext.stastics, all: data.length, pending: data.length, done: 0 })
-
-    }
-
-    const clearDoneTask = () => {
-        let data = [...dataContext.data]
-        const ln = data.length
-        data = data.filter((i) => i.state === false)
-        dataContext.changeHandler(data)
-
-        dataContext.statHandler({ all: data.length, pending: data.length, done: 0, deleted: dataContext.stastics.deleted + (ln - data.length) })
-    }
-    const clearAll = () => {
-        dataContext.statHandler({ all: 0, done: 0, pending: 0, deleted: dataContext.stastics.deleted + dataContext.data.length })
-        dataContext.changeHandler([])
-
-    }
-    const groupByAssignee = () => {
-        props.groupByAssignee()
-    }
-
-
-    const openHandler = (value) => {
-        setModelOpen(true)
-        handler.current = value
-    }
-    return <div className='control-buttons'>
-        <Button onClick={markAllHandler}>Mark All as Done</Button>
-        <Button onClick={marAsUndone}>Mark All as Un-Done</Button>
-        <Button onClick={() => {
-            openHandler({
-                title: 'Are sure for deleteing all tasks',
-                handler: clearDoneTask
-            })
-        }} >Clear Done Tasks</Button>
-        <Button onClick={() => {
-            openHandler({
-                title: 'Are sure for deleteing all tasks',
-                handler: clearAll
-            }
-            )
-        }}>Clear All Tasks</Button>
-        <Button onClick={groupByAssignee}>Group By Assignee</Button>
-        <Model isOpen={modelOpen} conformModel submitHandler={handler.current.handler} onClose={() => { setModelOpen(false) }} />
-
+  const openHandler = (value) => {
+    setModelOpen(true);
+    handler.current = value;
+  };
+  return (
+    <div className="control-buttons">
+      <Button onClick={markAllHandler}>Mark All as Done</Button>
+      <Button onClick={marAsUndone}>Mark All as Un-Done</Button>
+      <Button
+        onClick={() => {
+          openHandler({
+            title: "Are sure for deleteing all tasks",
+            handler: clearDoneTask,
+          });
+        }}
+      >
+        Clear Done Tasks
+      </Button>
+      <Button
+        onClick={() => {
+          openHandler({
+            title: "Are sure for deleteing all tasks",
+            handler: clearAll,
+          });
+        }}
+      >
+        Clear All Tasks
+      </Button>
+      <Button onClick={groupByAssignee}>Group By Assignee</Button>
+      <Model
+        isOpen={modelOpen}
+        conformModel
+        submitHandler={handler.current.handler}
+        onClose={() => {
+          setModelOpen(false);
+        }}
+      />
     </div>
-}
+  );
+};
 
-
-export default ControlButtons
+export default ControlButtons;
