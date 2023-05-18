be3c33c25fe913032ec0b247b3ae1f3a8ba4f846
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-18 17:47:21 +0300

    Using the theme provider to wrap the entire page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import HomePage from './pages/home';
-import Theme, { lightMode, darkMode } from './utils/Theme';
+import "./App.css";
+import HomePage from "./pages/home";
 
-import { useCallback, useContext, useState } from 'react';
+import { ThemeProvider } from "./contexts/Theme";
 function App() {
-  const [mode, setMode] = useState('light')
-
-  const siwtchHandler = () => {
-    setMode(mode === 'light' ? 'dark':'light')
-  }
-
   return (
-    <Theme.Provider value={{
-      mode: mode,
-      pallete: mode === 'light' ? lightMode : darkMode,
-      switchHandler:siwtchHandler
-    }}>
+    <ThemeProvider>
       <HomePage />
-    </Theme.Provider>
+    </ThemeProvider>
   );
 }
 
