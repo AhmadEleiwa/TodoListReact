2b0358ee5ca79e6e28a1b94b79ce31644145858f
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-24 17:47:24 +0300

    Updates

---
+++ precache-manifest.7096074b8898ca0f9ec43a1367d5a7c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb342349d5213a9d7349",
+    "url": "/TodoListReact/static/css/main.666cc50f.chunk.css"
+  },
+  {
+    "revision": "eb342349d5213a9d7349",
+    "url": "/TodoListReact/static/js/main.eb342349.chunk.js"
+  },
+  {
+    "revision": "3af0cecad8965df325fe",
+    "url": "/TodoListReact/static/js/1.3af0ceca.chunk.js"
+  },
+  {
+    "revision": "4f5f72d18cdec8a40389",
+    "url": "/TodoListReact/static/js/runtime~main.4f5f72d1.js"
+  },
+  {
+    "revision": "36d268b53baedfec9c59351ef28386f0",
+    "url": "/TodoListReact/index.html"
+  }
+];
